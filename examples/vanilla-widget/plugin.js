e8f7e5ddd7eed29b5b15108321c575c1c4c7e4e4
Michael Talbot <mike.talbot@alcumusgroup.com>
2021-09-13 05:37:48 +0100

    vanilla widget

---
+++ examples/vanilla-widget/plugin.js
@@ -0,0 +1,33 @@
+const {
+    Plugins: { register, PluginTypes }
+} = window.Framework4C
+
+register(PluginTypes.MAIN, "Article", editor, runtime)
+
+function editor({ settings, onChange, parent }) {
+    const div = document.createElement("div")
+    const label = document.createElement("label")
+    label.innerText = "Show Like Button"
+    const input = document.createElement("input")
+    input.type = "checkbox"
+    input.onchange = () => {
+        settings.showLikeButton = input.checked
+        onChange()
+    }
+    label.appendChild(input)
+    div.appendChild(label)
+    div.style.marginTop = "10px"
+    parent.innerHTML = ""
+    parent.appendChild(div)
+}
+
+function runtime({ parent, settings, article, user }) {
+    const div = document.createElement("div")
+    div.style.padding = "8px"
+    div.innerHTML = `
+        <h2>${article.title}</h2>
+        <h5>by ${user.displayName}</h5>
+    `
+    parent.innerHTML = ""
+    parent.appendChild(div)
+}
